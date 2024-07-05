b82981b65b2f00dcbeaefea5242a13c516ae8c38
Silvia Cárdenas <silvia2210102@correo.uis.edu.co>
2024-07-04 22:32:51 -0500

    Profile update

--- project/casuni/urls.py
+++ project/casuni/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),      # Example path for login view
     path('estudiante/', views.estudiante_home, name='estudiante_home'),
     path('propietario/', views.propietario_home, name='propietario_home'),
+    path('estudiante/perfil/<int:estudiante_id>/', views.estudiante_perfil, name='estudiante_perfil'),
+    path('propietario/perfil/<int:propietario_id>/', views.propietario_perfil, name='propietario_perfil'),
     path('alojamiento/<int:alojamiento_id>/', views.alojamiento_detalle, name='alojamiento_detalle'),
 ]
--- project/casuni/views.py
+++ project/casuni/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import LoginForm  # Ensure you have imported your updated LoginForm
 from django.contrib.auth.decorators import login_required
-from .models import Alojamiento
+from .models import Alojamiento, Estudiante, Propietario
 
 def home(request):
     print("home")
@@ -18,11 +18,9 @@ def login_view(request):
             if user is not None:
                 login(request, user)
                 if user.estudiante.exists():
-                    return redirect('estudiante_home')  # Redirige a la página de estudiante
+                    return redirect('estudiante_home')  
                 elif user.propietario.exists():
-                    return redirect('propietario_home')  # Redirige a la página de propietario
-                else:
-                    messages.error(request, 'Contraseña incorrecta.')
+                    return redirect('propietario_home')  
             else:
                 messages.error(request, 'Credenciales inválidas. Inténtelo de nuevo.')
     else:
@@ -31,22 +29,25 @@ def login_view(request):
 
 @login_required
 def estudiante_home(request):
-    return render(request, 'estudiante_home.html')
+    estudiante = get_object_or_404(Estudiante, user=request.user)
+    nombre_usuario = request.user.username
+    return render(request, 'estudiante_home.html', {'nombre_usuario': nombre_usuario, 'estudiante_id': estudiante.id})
 
 @login_required
 def propietario_home(request):
-    return render(request, 'propietario_home.html')
-
-@login_required  
-def estudiante_home(request):
+    propietario = get_object_or_404(Propietario, user=request.user)
     nombre_usuario = request.user.username
-    return render(request, 'estudiante_home.html', {'nombre_usuario': nombre_usuario})
-
-@login_required  
-def propietario_home(request):
-    nombre_usuario = request.user.username
-    return render(request, 'propietario_home.html', {'nombre_usuario': nombre_usuario})
+    return render(request, 'propietario_home.html', {'nombre_usuario': nombre_usuario, 'propietario_id': propietario.id})
 
 def alojamiento_detalle(request, alojamiento_id):
     alojamiento = get_object_or_404(Alojamiento, pk=alojamiento_id)
     return render(request, 'alojamiento_detalle.html', {'alojamiento': alojamiento})
+
+def estudiante_perfil(request,estudiante_id):
+    estudiante = get_object_or_404(Estudiante, id=estudiante_id)
+    return render(request, 'estudiante_perfil.html', {'estudiante': estudiante})
+
+def propietario_perfil(request, propietario_id):
+    propietario = get_object_or_404(Propietario, id=propietario_id)
+    alojamientos = Alojamiento.objects.filter(propietario=propietario)
+    return render(request, 'propietario_perfil.html', {'propietario': propietario, 'alojamientos': alojamientos})
